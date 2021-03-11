d837201d90216280358ebe1197270b77abcedbf8
Mateusz Konieczny <matkoniecz@gmail.com>
2021-03-11 21:51:18 +0100

    add repeated pattern

--- tactile_patterns_for_laser_cutter/data_format.py
+++ tactile_patterns_for_laser_cutter/data_format.py
@@ -166,9 +166,16 @@ def main():
         ])
     polygon = Polygon(outer)
     collection = Collection([polygon])
-    print(pretty_geojson_string(collection.to_geojson()))
+    #rint(pretty_geojson_string(collection.to_geojson()))
 
-    pattern = maze_under_construction_pattern(100, 20)
+    pattern = maze_under_construction_pattern(30, 30)
+    lat = 50.05518
+    lon = 19.92757
+    base_scaling = 0.000001
+    projection_scale = get_recommended_scaling(lat, lon, projection_code("web mercator"))
+    scale_lat = projection_scale['scale_lat'] * base_scaling
+    scale_lon = projection_scale['scale_lon'] * base_scaling
+    pattern.rescale(Rescale(multiply_lat=scale_lat, multiply_lon=scale_lon, add_lon=lon, add_lat=lat))
     print(pretty_geojson_string(pattern.to_geojson()))
 
 def maze_under_construction_pattern(repetition_x, repetition_y):
@@ -220,7 +227,17 @@ def maze_under_construction_pattern(repetition_x, repetition_y):
     l2 = Polygon(l2)
     v1 = Polygon(v1)
     v2 = Polygon(v2)
-    return Collection([l1, l2, v1, v2])
+    features = [l1, l2, v1, v2]
+    collection_group = Collection(features)
+    returned = Collection([])
+    for x in range(0, repetition_x):
+        for y in range(0, repetition_y):
+            x_move = (x - repetition_x/2) * multiplier * 2
+            y_move = (y - repetition_y/2) * multiplier * 2
+            added = copy.deepcopy(collection_group)
+            added.rescale(Rescale(multiply_x=1, multiply_y=1, add_x=x_move, add_y=y_move))
+            returned.merge_in_list(added.features())
+    return returned
 
 if __name__ == "__main__":
     main()
